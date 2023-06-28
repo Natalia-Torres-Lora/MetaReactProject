a082d4bcb9410257e3eef40b50240d0f654ee6e1
Natalia Torres <torresloranatalia@gmail.com>
2023-06-27 23:19:46 -0400

    Added the scroll effect for the header

--- my-portfolio/src/components/Header.js
+++ my-portfolio/src/components/Header.js
@@ -33,6 +33,32 @@ const socials = [
 ];
 
 const Header = () => {
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    let prevScrollPos = window.scrollY;
+
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      const headerElement = headerRef.current;
+      if (!headerElement) {
+        return;
+      }
+      if (prevScrollPos > currentScrollPos) {
+        headerElement.style.transform = "translateY(0)";
+      } else {
+        headerElement.style.transform = "translateY(-200px)";
+      }
+      prevScrollPos = currentScrollPos;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const handleClick = (anchor) => () => {
     const id = `${anchor}-section`;
     const element = document.getElementById(id);
@@ -46,6 +72,7 @@ const Header = () => {
 
   return (
     <Box
+      ref={headerRef}
       position="fixed"
       top={0}
       left={0}
@@ -68,8 +95,8 @@ const Header = () => {
             <HStack spacing={4}>
               {socials.map((social, index) => (
                 <a key={index} href={social.url}>
-                  <FontAwesomeIcon icon={social.icon} size ="2px" />
-                  </a>
+                  <FontAwesomeIcon icon={social.icon} size="2x" />
+                </a>
               ))}
             </HStack>
           </nav>
@@ -89,4 +116,5 @@ const Header = () => {
     </Box>
   );
 };
+
 export default Header;
