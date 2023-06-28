e2530efb88dd94d619aba52983233ee62b1647a9
Natalia Torres <torresloranatalia@gmail.com>
2023-06-27 23:14:33 -0400

    Added styled cards for the different projects

--- my-portfolio/src/components/Card.js
+++ my-portfolio/src/components/Card.js
@@ -7,7 +7,29 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <VStack
+      spacing={4}
+      borderWidth="1px"
+      borderColor="gray.200"
+      borderRadius="md"
+      p={4}
+      alignItems="felx=start"
+      backgroundColor="white"
+    >
+      <Image
+        src={imageSrc} atl={title} maxH="200px" objectFit="cover" />
+      <Heading as="h2" size="md" color="black" >
+        {title}
+      </Heading>
+      <Text color="grey">{description}</Text>
+      <HStack spacing={2} color="blue.500">
+        <Text>Learn More</Text>
+        <FontAwesomeIcon icon={faArrowRight} size="1x" />
+
+      </HStack>
+    </VStack>
+  );
 };
 
 export default Card;
